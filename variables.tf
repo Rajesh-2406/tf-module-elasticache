c2cf3e66105fd08a938dbad62871d0adc67ec8a6
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-30 12:21:15 +0530

    vpc ceration

--- variables.tf
+++ variables.tf
@@ -4,3 +4,13 @@ variable "component" {}
 variable "subnet_ids" {}
 variable "kms_key_arn" {}
 variable "sg_subnet_cidr" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "port" {
+  default = 6379
+}
+variable "vpc_id" {}
+variable "num_node_groups" {}
+variable "replicas_per_node_group" {}
