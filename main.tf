9b7abbb1a98b4321a0a3a55a7609657a713837fc
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-30 17:19:14 +0530

    vpc ceration

--- main.tf
+++ main.tf
@@ -35,9 +35,7 @@ resource "aws_elasticache_replication_group" "main" {
   replicas_per_node_group    = var.replicas_per_node_group
   subnet_group_name          = aws_elasticache_subnet_group.main.name
   parameter_group_name       = var.parameter_group_name
-  security_group_ids         = [ aws_security_group.main.id]
+  security_group_ids         = [aws_security_group.main.id]
   kms_key_id                 = var.kms_key_arn
   at_rest_encryption_enabled = true
-  skip_final_snapshot        = true
-
- }
+}
